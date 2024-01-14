fbb09a1574d1279b76a7409afd97d156621dc450
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-01-14 22:26:37 +0200

    Add files via upload

---
+++ Semester 1/Computer System Arhitecture /Laboratory 8/Write_in_file_from_keyboard_until_$.asm
@@ -0,0 +1,56 @@
+bits 32
+global start        
+extern exit, scanf, printf, fopen, fclose, fread, fprintf               
+import exit msvcrt.dll
+import scanf msvcrt.dll
+import printf msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+import fread msvcrt.dll
+import fprintf msvcrt.dll
+segment data use32 class=data
+    file_name db "writing_file.txt", 0
+    file_descriptor dd 0
+    access_write db "w", 0
+    text resb 50
+    format db "%s", 0
+segment code use32 class=code
+    start:
+        ; I interpreted it as reading only the character "$" or more specifically, that character being the first
+        create_file:
+            push dword access_write
+            push dword file_name
+            call [fopen]
+            add esp, 4 * 2
+            
+        mov [file_descriptor], eax
+        cmp eax, 0
+        je end_of_program    
+            
+        infinite_loop:
+            read_from_keyboard:
+                push dword text
+                push format
+                call [scanf]
+                add esp, 4 * 2
+            check_for_$:
+                mov esi, text
+                cmp byte[esi], "$"
+                je close_file
+            
+            write_in_file:
+                push dword text
+                push dword [file_descriptor]
+                call [fprintf]
+                add esp, 4 * 2
+            
+            jmp infinite_loop
+        
+        close_file:
+            push dword [file_descriptor]
+            call [fclose]
+            add esp, 4
+        
+        end_of_program:
+            push    dword 0
+            call    [exit]      
