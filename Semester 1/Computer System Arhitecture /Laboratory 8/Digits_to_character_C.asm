3f5f208dde82f23688aefab0d1922de0b16e2264
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-01-15 21:16:58 +0200

    Add files via upload

---
+++ Semester 1/Computer System Arhitecture /Laboratory 8/Digits_to_character_C.asm
@@ -0,0 +1,62 @@
+bits 32
+global start        
+extern exit, scanf, printf, fopen, fclose, fread, fprintf               
+import exit msvcrt.dll
+import scanf msvcrt.dll
+import printf msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+import fread msvcrt.dll
+import fprintf msvcrt.dll
+segment data use32 class=data
+    file_name db "output.txt", 0
+    file_descriptor dd 0
+    access_mode db "w", 0
+    text db "1234abcdABCD&!", 0
+    digits db "0123456789", 0
+segment code use32 class=code
+    start:
+        create_file:
+            push dword access_mode
+            push dword file_name
+            call [fopen]
+            add esp, 4 * 2
+        
+        mov [file_descriptor], eax
+        cmp eax, 0
+        je end_of_program
+        
+        loop_1:
+            cld
+            mov esi, text
+            prepare_checking:
+                lodsb
+                mov edi, digits
+                mov ecx, 10
+                start_checking:
+                    scasb
+                    jne not_digit
+                    
+                    dec esi
+                    mov byte[esi], 'C'
+                    inc esi
+                    
+                    not_digit:
+                loop start_checking
+                cmp al, 0
+                jne prepare_checking
+        
+        print_in_file:
+            push dword text
+            push dword [file_descriptor]
+            call [fprintf]
+            add esp, 4 * 2
+        
+        close_file:
+            push dword [file_descriptor]
+            call [fclose]
+            add esp, 4
+        
+        end_of_program:
+            push    dword 0
+            call    [exit]      
