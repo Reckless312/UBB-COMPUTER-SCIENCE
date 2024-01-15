76b36ef92cdf803121b8bb01b4d992539d3228f2
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-01-15 19:41:32 +0200

    Add files via upload

---
+++ Semester 1/Computer System Arhitecture /Laboratory 8/Odd_positions.asm
@@ -0,0 +1,82 @@
+bits 32
+global start        
+extern exit, scanf, printf, fopen, fclose, fread, fprintf               
+import exit msvcrt.dll
+import scanf msvcrt.dll
+import printf msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+import fread msvcrt.dll
+import fprintf msvcrt.dll
+segment data use32 class=data
+    file_name db "output.txt", 0
+    text db "12345678a", 0
+    file_descriptor dd 0
+    access_mode db "w", 0
+    digits db "0123456789", 0
+segment code use32 class=code
+    start:
+        create_file:
+            push dword access_mode
+            push dword file_name
+            call [fopen]
+            add esp, 4 * 2
+        
+        mov [file_descriptor], eax
+        
+        cmp eax, 0
+        je end_of_program
+        
+        change_to_position:
+            mov esi, text
+            mov eax, 0
+            start_:
+                cmp byte[esi], 0
+                je print_in_file
+                check_position:
+                    inc eax
+                    push eax
+                    cdq
+                    mov ebx, 2
+                    div ebx
+                    cmp edx, 1
+                    jne prepare_to_loop
+                
+                check_digit:
+                    mov edi, digits
+                    mov ecx, 10
+                    _start:
+                        mov al, [esi]
+                        scasb
+                        jne skip
+                        jmp change_digit
+                        skip:
+                    loop _start
+                    
+                    jmp prepare_to_loop
+                    
+                change_digit:
+                    pop eax
+                    mov byte[esi], 'X'
+                    inc esi
+                    jmp start_
+                
+                prepare_to_loop:
+                inc esi
+                pop eax
+                jmp start_
+        
+        print_in_file:
+            push dword text
+            push dword [file_descriptor]
+            call [fprintf]
+            add esp, 4 * 2
+        
+        close_file:
+            push dword[file_descriptor]
+            call [fclose]
+            add esp, 4
+    
+        end_of_program:
+            push    dword 0
+            call    [exit]      
