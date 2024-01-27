96a6bb752ed73730b64d5703bd9af30a4cd13059
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-01-27 23:45:51 +0200

    Update Main.py

--- Semester 1/Fundamentals of Programming/Hangman/Main.py
+++ Semester 1/Fundamentals of Programming/Hangman/Main.py
@@ -3,6 +3,7 @@ from Repository import Repository
 from Services import Services
 from UI import UI
 
+#Doesn't contain all of the specifications and unit tests.
 if __name__ == "__main__":
     repo = Repository()
     service = Services(repo)
