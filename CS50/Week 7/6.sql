524327a2a56323219038130bcbb6fda1840c64b5
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-07-26 10:56:52 +0300

    Delete CS50/Week 7 directory

--- CS50/Week 7/6.sql
+++
@@ -1,1 +0,0 @@
-SELECT name FROM songs WHERE songs.artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');
--- CS50/Week 7/7.sql
+++
@@ -1,1 +0,0 @@
-SELECT AVG(energy) FROM songs WHERE songs.artist_id = (SELECT id FROM artists WHERE name = 'Drake');
