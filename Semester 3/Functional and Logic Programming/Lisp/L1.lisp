7ce15ab412fe1ae8ee3a9624918fa0aff7aa3a66
Reckless312 <145335181+Reckless312@users.noreply.github.com>
2024-12-11 02:36:05 +0200

    Update L1.lisp

--- Semester 3/Functional and Logic Programming/Lisp/L1.lisp
+++ Semester 3/Functional and Logic Programming/Lisp/L1.lisp
@@ -21,7 +21,7 @@
 
 ; flow model (i, o)
 ; l1 - list
-(defun lengthoflist(l1)
+(defun lengthoflist (l1)
     (cond
         ((null l1) 0)
         (t (+ (lengthoflist (cdr l1)) 1))
@@ -33,24 +33,75 @@
 (defun maxoflist(l1)
     (cond
         ((null l1) nil)
-        ((numberp (car l1)) (let ((recursivemax(maxoflist (cdr l1))))
+        ((numberp (car l1))
             (cond
-                ((null recursivemax) (car l1))
-                ((> (car l1) recursivemax) (car l1))
-                (t recursivemax)
+                ((null (maxoflist (cdr l1))) (car l1))
+                ((> (car l1) (maxoflist (cdr l1))) (car l1))
+                (t (maxoflist (cdr l1)))
             )
-        ))
-        ((listp (car l1)) (let ((recursivemax(maxoflist (cdr l1))))
-            (let ((currentmax(maxoflist (car l1))))
-                (cond
-                    ((null recursivemax) currentmax)
-                    ((null currentmax) recursivemax)
-                    ((> currentmax recursivemax) currentmax)
-                    (t recursivemax)
-                )
+        )
+        ((listp (car l1)) 
+            (cond
+                ((null (maxoflist (cdr l1))) (maxoflist (car l1)))
+                ((null (maxoflist (car l1))) (maxoflist (cdr l1)))
+                ((> (maxoflist (car l1)) (maxoflist (cdr l1))) (maxoflist (car l1)))
+                (t (maxoflist (cdr l1)))
             )
-        ))
+        )
         (t (maxoflist (cdr l1)))
     )
 )
 ; (maxoflist '(1 2 -1 a () ((a (b 5))) 3 ((-2) 1)))
+
+; l1 - list that contains operators and numbers
+
+(defun preorderArithmetic (l1)
+    (cond
+        ((numberp (car l1)) (list (car l1) (cdr l1)))
+        ((member (car l1) '(+ - * /))
+            (list (funcall (car l1)
+                (car (preorderArithmetic (cdr l1))) 
+                (car (preorderArithmetic (cadr (preorderArithmetic (cdr l1)))))
+             )
+                (cadr (preorderArithmetic (cadr (preorderArithmetic (cdr l1)))))
+             )            
+        )
+    )
+)
+
+; l1 - list that contains operators and numbers
+
+(defun mainpreorderArithmetic (l1)
+    (cond
+        ((null (cadr (preorderArithmetic l1))) (car (preorderArithmetic l1)))
+        (t nil)
+    )
+)
+
+;(mainpreorderArithmetic '(+ * 2 4 - 5 * 2 2))
+;(mainpreorderArithmetic '(+ * 2 4 - 2 5 * 2 2))
+;(mainpreorderArithmetic '(+ * 2 4 - 5 * 2 2))
+;(mainpreorderArithmetic '(+ 1 3))
+;(mainpreorderArithmetic '(+ * 2 4 3))
+
+(defun weirdsum (l1)
+    (cond
+        ((null l1 ) 0)
+        ((numberp (car l1))
+             (if (= (mod (car l1) 2) 0) 
+                (+ (weirdsum (cdr l1)) (- (car l1) 1)) 
+                (+ (weirdsum (cdr l1)) (car l1))))
+        (t (weirdsum (cdr l1)))
+    )
+)
+
+(defun even (l1)
+    (cond
+        ((null l1) nil)
+        ((= (mod (weirdsum l1) 2) 0) T) 
+        ((= (mod (weirdsum l1) 2) 1) nil)
+    )
+)
+
+;(even '(1 2 3 4 (a b (1 2 3) a ) 2))
+;(even '(1 2 3 4 (a b (1 2 3) a ) 2 2))
