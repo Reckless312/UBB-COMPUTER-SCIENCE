55d9bcfea34e6185dd6aed86ad83a6825e5a0148
Reckless312 <ionutcora66@gmail.com>
2025-03-09 13:53:53 +0200

    navigation component updated

--- Semester 4/Systems for Design and Implementation/mygamelist/app/components/Navbar.tsx
+++ Semester 4/Systems for Design and Implementation/mygamelist/app/components/Navbar.tsx
@@ -1,14 +1,77 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import {auth, signIn, signOut} from "@/auth";
 
-const Navbar = () => {
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/app/components/dropdown-menu"
+
+const Navbar = async () => {
+    const session = await auth();
+
     return (
-        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-            <nav className="flex justify-between items-center">
+        <header className="bg-black px-4">
+            <nav className="flex items-center justify-between">
                 <Link href="/">
                     <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
+                <div className="flex items-center justify-between text-white">
+                    {session && session.user ? (
+                        <>
+                            <div className="flex items-center justify-between px-2">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <Image src="/menu.png" alt="main menu" width={30} height={30}></Image>
+                                        </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>Others</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            My List
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                                <span>{session?.user?.name}</span>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <Image src="/arrow_drop_down.png" alt="menu drop down" width={30} height={30}></Image>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            <form action={async () => {
+                                                "use server";
+
+                                                await signOut({redirectTo: "/"});
+                                            }}>
+                                                <button type="submit">
+                                                    Logout
+                                                </button>
+                                            </form></DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                                <Image src={session?.user?.image || "/logo.png"} alt="profile picture" width={30} height={30}></Image>
+                            </div>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            "use server";
+
+                            await signIn('github');
+                        }}>
+                            <button className="text-white bg-green-400 px-8 py-0.1 rounded-2xl cursor-pointer" type="submit">
+                                Login
+                            </button>
+                        </form>
+                    )}
+                </div>
             </nav>
         </header>
     )
--- Semester 4/Systems for Design and Implementation/mygamelist/next.config.ts
+++ Semester 4/Systems for Design and Implementation/mygamelist/next.config.ts
@@ -5,3 +5,9 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+    images: {
+        domains: ['avatars.githubusercontent.com'],
+    },
+};
