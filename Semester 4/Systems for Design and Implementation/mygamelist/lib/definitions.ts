846f7173b6c9719367ae0ed4cf892acf5fff23dd
Cora Ioan-Alexandru <ionutcora66@gmail.com>
2025-03-14 22:47:46 +0200

    Delete Semester 4/Systems for Design and Implementation/mygamelist directory

--- Semester 4/Systems for Design and Implementation/mygamelist/lib/definitions.ts
+++
@@ -1,8 +0,0 @@
-export type Games = {
-    id: number;
-    name: string;
-    status: 'plan-to-play' | 'playing' | 'completed';
-    score: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-    startedDate: Date;
-    finishedDate: Date;
-};
