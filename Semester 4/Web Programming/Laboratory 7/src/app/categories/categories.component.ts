1b7ffc6ff770a2ef8f2d4777f8d017f11a42be8a
Reckless312 <ionutcora66@gmail.com>
2025-06-15 21:47:04 +0300

    pagination conquered

--- Semester 4/Web Programming/Laboratory 7/src/app/categories/categories.component.ts
+++ Semester 4/Web Programming/Laboratory 7/src/app/categories/categories.component.ts
@@ -2,10 +2,13 @@ import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {CategoriesService} from '../services/categories.service';
 import {Category} from '../model/category.type';
+import {NavigationComponent} from '../components/navigation/navigation.component';
 @Component({
   selector: 'app-categories',
+  standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NavigationComponent
   ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
@@ -16,39 +19,24 @@ export class CategoriesComponent implements OnInit {
   categories = signal<Array<Category>>([])
   currentPage = signal<number>(1)
 
-  lastPage = signal<number>(1)
-
-  allCategories: Array<Category> = [];
+  allCategories = signal<Array<Category>>([]);
   categoriesOnPage = 3;
 
   ngOnInit(): void {
       this.categoryService.getCategories().subscribe(categories => {
-        this.allCategories = categories
-        this.lastPage.set(Math.ceil(this.allCategories.length / this.categoriesOnPage))
-        this.displayCategories()
+        this.allCategories.set(categories);
+        this.displayCategories();
       })
   }
 
   displayCategories() {
     const start = (this.currentPage() - 1) * this.categoriesOnPage;
     const end = this.currentPage() * this.categoriesOnPage;
-    this.categories.set(this.allCategories.slice(start, end))
+    this.categories.set(this.allCategories().slice(start, end))
   }
 
-  nextPage() {
-    if(this.currentPage() < this.lastPage()){
-      this.currentPage.update(page => page + 1);
-      this.displayCategories();
-    }
+  onPageChange(page: number) {
+    this.currentPage.set(page);
+    this.displayCategories();
   }
-
-  prevPage() {
-    if(this.currentPage() > 1){
-      this.currentPage.update(page => page - 1);
-      this.displayCategories();
-    }
-  }
-
-  isFirstPage = computed(() => this.currentPage() === 1)
-  isLastPage = computed(() => this.currentPage() === this.lastPage())
 }
---
+++ Semester 4/Web Programming/Laboratory 7/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,32 @@
+import {Component, computed, EventEmitter, Input, input, Output, output} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+  currentPage = input(1)
+  totalItems = input(0)
+  itemsPerPage = input(3)
+
+  @Output() pageChange = new EventEmitter<number>()
+
+  lastPage = computed(() => Math.ceil(this.totalItems() / this.itemsPerPage()))
+  isFirstPage = computed(() => this.currentPage() === 1)
+  isLastPage = computed(() => this.currentPage() === this.lastPage())
+
+  onNext() {
+    if (!this.isLastPage()) {
+      this.pageChange.emit(this.currentPage() + 1)
+    }
+  }
+
+  onPrev() {
+    if (!this.isFirstPage()) {
+      this.pageChange.emit(this.currentPage() - 1)
+    }
+  }
+}
