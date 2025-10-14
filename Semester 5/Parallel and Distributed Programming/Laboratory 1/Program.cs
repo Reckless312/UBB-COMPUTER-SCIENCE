e0b08c9c3561774d293135552e68bd99d7777874
Cora Ioan-Alexandru <ionutcora@proton.me>
2025-10-14 21:52:03 +0300

    Lab 1 and 2 PPD done

--- Semester 5/Parallel and Distributed Programming/Laboratory 1/Program.cs
+++ Semester 5/Parallel and Distributed Programming/Laboratory 1/Program.cs
@@ -1,196 +1,184 @@
-﻿public class Laboratory
+﻿// Threads | Warehouses | Products | Tasks | Time
+// 100 | 10 | 20 | 10000 | 2.02 sec
+// 10 | 10 | 20 | 10000 | 1.01 sec
+// 150 | 10 | 20 | 10000 | 4.03 sec
+// 150 | 100 | 20 | 10000 | 3.04 sec
+
+const int numberOfThreads = 150;
+const int numberOfProducts = 3;
+const int numberOfTask = 10000;
+const int numberOfWarehouses = 2;
+const int randomizerUpperBound = 100;
+
+int [,] warehousesGoods = new int[numberOfWarehouses, numberOfProducts];
+int [] initialStock = new int[numberOfProducts];
+
+Mutex [,] mutexes = new Mutex[numberOfWarehouses, numberOfProducts];
+
+List<Thread> threads = new List<Thread>();
+
+void InitializeWarehouses()
 {
-    private static int randomizerUpperBound = 100;
-    private static int numberOfTask = 10000;
-    private static int numberOfThreads = 100;
-    private static int numberOfProducts = 200;
-    
-    private static void Main()
+    Random randomizer = new Random();
+        
+    for (int columnIndex = 0; columnIndex < numberOfProducts; columnIndex++)
     {
-        /* Solution:
-         * For simplicity the values for the threads / products / tasks are hardcoded
-         * A worker thread is a warehouse
-         * Each warehouse has an array of products (matrix cause OOP is ugly)
-         * Matrix of mutexes for concurrency control
-         */
-        
-        int [,] warehousesGoods = new int[Laboratory.numberOfThreads, Laboratory.numberOfProducts];
-        int [] initialStock = new int[Laboratory.numberOfProducts];
-        
-        Mutex [,] mutexes = new Mutex[Laboratory.numberOfThreads, Laboratory.numberOfProducts];
-        
-        List<Thread> threads = new List<Thread>();
-
-        
-        // No point to move from the same warehouse to the same warehouse or nothing to nothing :>
-        if (numberOfThreads < 2)
+        for (int rowIndex = 0; rowIndex < numberOfWarehouses; rowIndex++)
         {
-            return;
-        }
-        
-        InitializeRandomlyTheWarehouses(warehousesGoods, initialStock);
-        InitializeMutexes(mutexes);
-        InitializeThreads(threads, warehousesGoods, mutexes);
-        
-        threads.ForEach(thread => thread.Start());
-        
-        Thread inspectorThread = new Thread(() => InspectorThread(warehousesGoods, initialStock, threads, mutexes));
-        inspectorThread.Start();
-        
-        try
-        {
-            threads.ForEach(thread => thread.Join());
-            inspectorThread.Join();
-        }
-        catch (Exception e)
-        {
-            System.Console.WriteLine(e);
-        }
-    }
-
-    public static void InitializeRandomlyTheWarehouses(int [,] warehousesGoods, int[] initialStock)
-    {
-        Random randomizer = new Random();
-        
-        for (int columnIndex = 0; columnIndex < Laboratory.numberOfProducts; columnIndex++)
-        {
-            for (int rowIndex = 0; rowIndex < Laboratory.numberOfThreads; rowIndex++)
-            {
-                int randomQuantity = randomizer.Next(Laboratory.randomizerUpperBound);
-                warehousesGoods[rowIndex, columnIndex] = randomQuantity;
+            int randomQuantity = randomizer.Next(randomizerUpperBound);
+            warehousesGoods[rowIndex, columnIndex] = randomQuantity;
                 
-                initialStock[columnIndex] += randomQuantity;
-            }
+            initialStock[columnIndex] += randomQuantity;
         }
-    }
-
-    public static void InitializeMutexes(Mutex[,] mutexes)
-    {
-        for (int rowIndex = 0; rowIndex < numberOfThreads; rowIndex++)
-        {
-            for (int columnIndex = 0; columnIndex < numberOfProducts; columnIndex++)
-            {
-                mutexes[rowIndex, columnIndex] = new Mutex();
-            }
-        }
-    }
-
-    public static void InitializeThreads(List<Thread> threads, int[,] warehousesGoods, Mutex[,] mutexes)
-    {
-        for (int index = 0; index < numberOfThreads; index++)
-        {
-            int threadId = index;
-            
-            Thread workThread = new Thread(() => WorkThread(threadId, warehousesGoods, mutexes));
-            
-            threads.Add(workThread);
-        }
-    }
-
-    public static int[] GetNewTotalStock(int[,] warehousesGoods, Mutex[,] mutexes)
-    {
-        int[] newStock = new int[numberOfProducts];
-        
-        // Locking everything so no thread has access to some of the stock
-        for (int j = 0; j < numberOfProducts; j++)
-        {
-            for (int i = 0; i < numberOfThreads; i++)
-            {
-                mutexes[i, j].WaitOne();
-            }
-        }
-        
-        // Both adding and releasing should be fine since the threads will only work with what is unlocked
-        for (int j = 0; j < numberOfProducts; j++)
-        {
-            for (int i = 0; i < numberOfThreads; i++)
-            {
-                newStock[j] += warehousesGoods[i, j];
-                mutexes[i, j].ReleaseMutex();
-            }
-        }
-        
-        return newStock;
-    }
-
-    public static void WorkThread(int threadId, int[,] warehousesGoods, Mutex [,] mutexes)
-    {
-        int numberRows = warehousesGoods.GetLength(0);
-        int numberColumns = warehousesGoods.GetLength(1);
-        
-        Random randomizer = new Random();
-        
-        for (int i = 0; i < Laboratory.numberOfTask; i++)
-        {
-            int randomWarehouse = randomizer.Next(numberRows);
-
-            while (randomWarehouse == threadId)
-            {
-                randomWarehouse = randomizer.Next(numberRows);
-            }
-            
-            int randomProduct = randomizer.Next(numberColumns);
-            
-            // Locking in an increasing order of id to avoid deadlocks
-            int firstLockedThread, secondLockedThread;
-            
-            if (threadId < randomWarehouse)
-            {
-                firstLockedThread = threadId;
-                secondLockedThread = randomWarehouse;
-            }
-            else
-            {
-                firstLockedThread = randomWarehouse;
-                secondLockedThread = threadId;
-            }
-            
-            mutexes[firstLockedThread, randomProduct].WaitOne();
-            mutexes[secondLockedThread, randomProduct].WaitOne();
-
-            if (warehousesGoods[threadId, randomProduct] == 0)
-            {
-                // Nothing to do
-                mutexes[secondLockedThread, randomProduct].ReleaseMutex();
-                mutexes[firstLockedThread, randomProduct].ReleaseMutex();
-                continue;
-            }
-            
-            warehousesGoods[firstLockedThread, randomProduct]--;
-            warehousesGoods[secondLockedThread, randomProduct]++;
-            
-            mutexes[secondLockedThread, randomProduct].ReleaseMutex();
-            mutexes[firstLockedThread, randomProduct].ReleaseMutex();
-        }
-    }
-    
-    public static void InspectorThread(int[,] warehousesGoods, int[] initialStock, List<Thread> threads, Mutex[,] mutexes)
-    {
-        bool areWorkersDone = false;
-        DateTime startTime = DateTime.Now;
-        while (!areWorkersDone)
-        {
-            Thread.Sleep(1000);
-            
-            areWorkersDone = true;
-            threads.ForEach(thread =>
-            {
-                if (thread.IsAlive)
-                {
-                    areWorkersDone = false;
-                }
-            });
-            
-            int[] newStock = GetNewTotalStock(warehousesGoods, mutexes);
-
-            for (int i = 0; i < numberOfProducts; i++)
-            {
-                if (initialStock[i] != newStock[i])
-                {
-                    System.Console.WriteLine("Something is not right! " + initialStock[i] + " != " + newStock[i]);
-                }
-            }
-        }
-        System.Console.WriteLine("Done in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
     }
 }
+
+void InitializeMutexes()
+{
+    for (int rowIndex = 0; rowIndex < numberOfWarehouses; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < numberOfProducts; columnIndex++)
+        {
+            mutexes[rowIndex, columnIndex] = new Mutex();
+        }
+    }
+}
+
+void InitializeThreads()
+{
+    for (int index = 0; index < numberOfThreads; index++)
+    {
+        int threadId = index;
+            
+        Thread workThread = new Thread(() => WorkThread());
+            
+        threads.Add(workThread);
+    }
+}
+
+int[] CalculateStock()
+{
+    int[] newStock = new int[numberOfProducts];
+    
+    for (int j = 0; j < numberOfProducts; j++)
+    {
+        for (int i = 0; i < numberOfWarehouses; i++)
+        {
+            mutexes[i, j].WaitOne();
+        }
+    }
+    
+    for (int j = 0; j < numberOfProducts; j++)
+    {
+        for (int i = 0; i < numberOfWarehouses; i++)
+        {
+            newStock[j] += warehousesGoods[i, j];
+            mutexes[i, j].ReleaseMutex();
+        }
+    }
+        
+    return newStock;
+}
+
+void WorkThread()
+{
+    Random randomizer = new Random();
+        
+    for (int i = 0; i < numberOfTask; i++)
+    {
+        int sendingWarehouse = randomizer.Next(numberOfWarehouses);
+        int receivingWarehouse = randomizer.Next(numberOfWarehouses);
+
+        while (sendingWarehouse == receivingWarehouse)
+        {
+            receivingWarehouse = randomizer.Next(numberOfWarehouses);
+        }
+        
+        int randomProduct = randomizer.Next(numberOfProducts);
+        
+        int firstLockedWarehouse, secondLockedWarehouse;
+            
+        if (receivingWarehouse < sendingWarehouse)
+        {
+            firstLockedWarehouse = receivingWarehouse;
+            secondLockedWarehouse = sendingWarehouse;
+        }
+        else
+        {
+            firstLockedWarehouse = sendingWarehouse;
+            secondLockedWarehouse = receivingWarehouse;
+        }
+            
+        mutexes[firstLockedWarehouse, randomProduct].WaitOne();
+        mutexes[secondLockedWarehouse, randomProduct].WaitOne();
+
+        if (warehousesGoods[sendingWarehouse, randomProduct] == 0)
+        {
+            mutexes[secondLockedWarehouse, randomProduct].ReleaseMutex();
+            mutexes[firstLockedWarehouse, randomProduct].ReleaseMutex();
+            continue;
+        }
+        
+        int randomQuantity = randomizer.Next(warehousesGoods[firstLockedWarehouse, randomProduct]);
+            
+        warehousesGoods[firstLockedWarehouse, randomProduct] -= randomQuantity;
+        warehousesGoods[secondLockedWarehouse, randomProduct] += randomQuantity;
+            
+        mutexes[secondLockedWarehouse, randomProduct].ReleaseMutex();
+        mutexes[firstLockedWarehouse, randomProduct].ReleaseMutex();
+    }
+}
+
+void InspectorThread()
+{
+    DateTime startTime = DateTime.Now;
+    
+    bool areWorkersDone = false;
+    
+    while (!areWorkersDone)
+    {
+        Thread.Sleep(1000);
+            
+        areWorkersDone = true;
+        threads.ForEach(thread =>
+        {
+            if (thread.IsAlive)
+            {
+                areWorkersDone = false;
+            }
+        });
+            
+        int[] newStock = CalculateStock();
+
+        for (int i = 0; i < numberOfProducts; i++)
+        {
+            if (initialStock[i] != newStock[i])
+            {
+                System.Console.WriteLine("Something is not right! " + initialStock[i] + " != " + newStock[i]);
+            }
+        }
+    }
+    System.Console.WriteLine("Done in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
+}
+
+void Main()
+{
+    if (numberOfWarehouses < 2)
+    {
+        return;
+    }
+    
+    InitializeWarehouses();
+    InitializeMutexes();
+    InitializeThreads();
+    
+    threads.ForEach(thread => thread.Start());
+
+    Thread inspectorThread = new Thread(() => InspectorThread());
+    inspectorThread.Start();
+    
+    threads.ForEach(thread => thread.Join());
+    inspectorThread.Join();
+}
+
+Main();
--- Semester 5/Parallel and Distributed Programming/Laboratory 2/Program.cs
+++ Semester 5/Parallel and Distributed Programming/Laboratory 2/Program.cs
@@ -1,7 +1,87 @@
-﻿class Program
+﻿// For queue size = 1 -> around 719 ms
+// For queue size = 10 -> around 97 ms
+// For queue size = 100 -> around 38 ms
+// No further improvements for the vector size (100.000); 100 seems to be the sweet spot
+
+int vectorSize = 100000;
+int queueSize = 100;
+int randomizerUpperBound = 100;
+
+int[] firstVector = new int[vectorSize];
+int[] secondVector = new int[vectorSize];
+
+Queue<int> queue = new Queue<int>(queueSize);
+
+void InitializeVector(int[] vector)
 {
-    public static void Main()
+    Random randomizer = new Random();
+    
+    for (int i = 0; i < vectorSize; i++)
     {
-        
+        vector[i] = randomizer.Next(randomizerUpperBound);
     }
 }
+
+void Producer()
+{
+    for (int i = 0; i < vectorSize; i++)
+    {
+        int product = firstVector[i] * secondVector[i];
+        
+        lock (queue)
+        {
+            while (queue.Count == queueSize)
+            {
+                Monitor.Wait(queue);
+            }
+            
+            queue.Enqueue(product);
+            Monitor.Pulse(queue);
+        }
+    }
+}
+
+void Consumer()
+{
+    int sum = 0;
+    for (int i = 0; i < vectorSize; i++)
+    {
+        lock (queue)
+        {
+            while (queue.Count == 0)
+            {
+                Monitor.Wait(queue);
+            }
+
+            int receivedProduct = queue.Dequeue();
+            sum += receivedProduct;
+            Monitor.Pulse(queue);
+        }
+    }
+    Console.WriteLine("Final sum: " + sum);
+}
+
+int Main()
+{
+    InitializeVector(firstVector);
+    InitializeVector(secondVector);
+
+    Thread producer = new Thread(Producer);
+    Thread consumer = new Thread(Consumer);
+    
+    DateTime startTime = DateTime.Now;
+    
+    producer.Start();
+    consumer.Start();
+    
+    producer.Join();
+    consumer.Join();
+    
+    DateTime endTime = DateTime.Now;
+    
+    Console.WriteLine("Time elapsed: " + (endTime - startTime).TotalMilliseconds + " ms");
+    
+    return 0;
+}
+
+Main();
