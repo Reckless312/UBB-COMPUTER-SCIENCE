5802cae9fcb46d6e3ea940377bb2944450beb505
Cora Ioan-Alexandru <ionutcora@proton.me>
2025-11-11 18:52:34 +0200

    Update lab.py

--- Semester 5/Public Key Cryptography/Laboratory 3/lab.py
+++ Semester 5/Public Key Cryptography/Laboratory 3/lab.py
@@ -2,7 +2,7 @@ import random
 from sieve import sieve
 from gcd_binary import gcd_binary
 
-def find_k(b):
+def find_k(b=13):
     k = 1
     for prime in sieve(b):
         copy = prime
@@ -12,7 +12,7 @@ def find_k(b):
 
     return k
 
-def pollard_p1(n, k, b):
+def pollard_p1(n, k, b=13):
     while True:
         a_choices = []
         for i in range(2, b - 1):
@@ -29,12 +29,20 @@ def pollard_p1(n, k, b):
             if gcd != 1 and gcd != n:
                 return gcd, n // gcd
 
-        b *= 2
+        return -1, -1
+        # Commenting this next line cause I don't get if the algorithm should go until it finds the factor (it probably won't :< )
+        #b *= 2
 
 
 if __name__ == "__main__":
     number = 1241143
-    bound = 13
-    k = find_k(bound)
-    gcd, n = pollard_p1(number, k, bound)
+    try:
+        bound = int(input("Enter the bound: "))
+        k = find_k(bound)
+        gcd, n = pollard_p1(number, k, bound)
+    except Exception:
+        k = find_k()
+        gcd, n = pollard_p1(number, k)
+
+    #-1, -1 -> need a better bound
     print(gcd, n)
